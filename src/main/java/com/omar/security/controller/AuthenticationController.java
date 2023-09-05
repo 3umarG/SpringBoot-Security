8c3ccce9b42edceb6612b898b581373e58a034c8
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-06 01:22:05 +0200

    Add `/confirm` endpoint.

--- src/main/java/com/omar/security/controller/AuthenticationController.java
+++ src/main/java/com/omar/security/controller/AuthenticationController.java
@@ -3,10 +3,7 @@ package com.omar.security.controller;
 import com.omar.security.dao.response.LoginResponse;
 import com.omar.security.dao.response.RegisterResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.omar.security.dao.request.SignUpRequest;
 import com.omar.security.dao.request.LoginRequest;
@@ -14,18 +11,25 @@ import com.omar.security.service.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.concurrent.TimeoutException;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     @PostMapping("/signup")
-    public ResponseEntity<RegisterResponse> signup(@RequestBody SignUpRequest request) {
+    public ResponseEntity<RegisterResponse> signup(@RequestBody SignUpRequest request) throws TimeoutException {
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authenticationService.login(request));
+    }
+
+    @GetMapping("/confirm")
+    public ResponseEntity<?> confirm(@RequestParam String token){
+        return ResponseEntity.ok(authenticationService.confirm(token));
     }
 }
