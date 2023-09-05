dcf975e2b52b009a9fb2d95961f37961cc0b6346
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-06 01:21:05 +0200

    Add `setConfirmedAt` & `getToken` to `ConfirmationTokenService`.

--- src/main/java/com/omar/security/service/ConfirmationTokenService.java
+++ src/main/java/com/omar/security/service/ConfirmationTokenService.java
@@ -6,7 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 public interface ConfirmationTokenService {
 
     void save(ConfirmationToken confirmationToken);
+
+    Optional<ConfirmationToken> getToken(String token);
+
+    int setConfirmedAt(String token);
 }
--- src/main/java/com/omar/security/service/impl/ConfirmationTokenServiceImpl.java
+++ src/main/java/com/omar/security/service/impl/ConfirmationTokenServiceImpl.java
@@ -6,6 +6,9 @@ import com.omar.security.service.ConfirmationTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ConfirmationTokenServiceImpl implements ConfirmationTokenService {
@@ -16,5 +19,16 @@ public class ConfirmationTokenServiceImpl implements ConfirmationTokenService {
     @Override
     public void save(ConfirmationToken confirmationToken) {
         repository.save(confirmationToken);
+    }
+
+    @Override
+    public Optional<ConfirmationToken> getToken(String token) {
+        return repository.findByToken(token);
+    }
+
+    @Override
+    public int setConfirmedAt(String token) {
+        return repository.updateConfirmedAt(
+                token, LocalDateTime.now());
     }
 }
