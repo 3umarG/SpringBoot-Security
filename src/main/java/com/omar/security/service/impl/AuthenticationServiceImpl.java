4fec717c76da92360d7092bc2adb3df77d5c5aad
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-05 20:59:14 +0200

    Use `ConfirmationTokenService` while signingUp new user.

--- src/main/java/com/omar/security/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/omar/security/service/impl/AuthenticationServiceImpl.java
@@ -2,7 +2,11 @@ package com.omar.security.service.impl;
 
 import com.omar.security.dao.response.LoginResponse;
 import com.omar.security.dao.response.RegisterResponse;
+import com.omar.security.entities.ConfirmationToken;
 import com.omar.security.exceptions.NotFoundAuthenticatedUserException;
+import com.omar.security.service.ConfirmationTokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -20,6 +24,9 @@ import com.omar.security.service.JwtService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -27,9 +34,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final ConfirmationTokenService confirmationTokenService;
+
+    private Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
 
     @Override
     public RegisterResponse signup(SignUpRequest request) {
+
+        // create user with given information
         var user = User.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
@@ -38,8 +50,25 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .role(Role.ROLE_USER)
                 .build();
 
+        // save the user to db
         userRepository.save(user);
 
+        // create confirmation token
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(1), // TODO : make that more long
+                user);
+
+        // save confirmation token
+        confirmationTokenService.save(confirmationToken);
+        logger.info(confirmationToken.getToken());
+
+        // TODO : SEND EMAIL WITH THAT TOKEN
+
+
+        // make the response of the request
         return RegisterResponse.builder()
                 .userName(user.getEmail())
                 .email(user.getEmail())
@@ -48,7 +77,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     @Override
-    public LoginResponse login(LoginRequest request) throws NotFoundAuthenticatedUserException{
+    public LoginResponse login(LoginRequest request) throws NotFoundAuthenticatedUserException {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -56,7 +85,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                             request.getPassword()
                     )
             );
-        }catch (AuthenticationException exception){
+        } catch (AuthenticationException exception) {
             throw new NotFoundAuthenticatedUserException("Not Authenticated User");
         }
 
