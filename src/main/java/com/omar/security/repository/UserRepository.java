0e6ba7116d85241ba49a4d192d35fb91ec890967
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-06 01:18:35 +0200

    Create `enableAppUser` in `UserRepository`.

--- src/main/java/com/omar/security/repository/UserRepository.java
+++ src/main/java/com/omar/security/repository/UserRepository.java
@@ -3,12 +3,22 @@ package com.omar.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.omar.security.entities.User;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     // Since email is unique, we'll find users by email
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User a " +
+           "SET a.enabled = TRUE " +
+           "WHERE a.email = ?1")
+    int enableAppUser(String email);
 }
